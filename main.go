380fd7d8a45cfc0de9ec933193ee6e1c462dd878
zuozhen666 <1774098040@qq.com>
2022-04-19 18:21:06 +0800

    trash code

--- main.go
+++ main.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"RaftKV/raft"
+	"net/http"
 	"os"
 )
 
 func main() {
-	// httpServer := &http.Server{
-	// 	Addr: ":8080",
-	// 	Handler: &kvServer{
-	// 		store: NewKvStore(),
-	// 	},
-	// }
-	// httpServer.ListenAndServe()
 	args := os.Args[1:]
 	if len(args) < 1 {
 		panic("args not fail")
 	}
+	httpServer := &http.Server{
+		Addr: args[0],
+		Handler: &kvServer{
+			store: NewKvStore(),
+		},
+	}
+	httpServer.ListenAndServe()
 	client := raft.NewRaftClient()
-	r := raft.NewRaft(args[0], args[1:], client.RequestVote, client.AppendEntries)
+	r := raft.NewRaft(args[1], args[2:], client.RequestVote, client.AppendEntries)
 	server := raft.NewRaftServer(r)
-	server.Start(args[0])
+	server.Start(args[1])
 }
